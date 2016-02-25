af333252136c88bae2374edea556715c6c72adbc
Geesun Xu <geesun@gmail.com>
2016-02-25 15:16:19 +0800

    add condition compile

---
+++ src/debug.c
@@ -0,0 +1,8 @@
+#include <config.h>
+#include <stdio.h>
+
+int main (void)
+{
+    printf("Hello debug "PACKAGE_STRING"\n");
+    return 0;
+}
---
+++ src/hello-debug.c
@@ -0,0 +1,7 @@
+#include <config.h>
+#include <stdio.h>
+
+int hello()
+{
+    printf("hello linux \n");
+}
