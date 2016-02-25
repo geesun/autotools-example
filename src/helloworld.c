d2d122bbd1f5aaec6167271a8d51ac726ea55d59
Geesun Xu <geesun@gmail.com>
2016-02-25 14:11:21 +0800

    add with/without/enable/disable option example in configure.ac

--- src/helloworld.c
+++ src/helloworld.c
@@ -3,6 +3,20 @@
 
 int main (void)
 {
-    printf("Hello "PACKAGE_STRING);
+    printf("Hello "PACKAGE_STRING"\n");
+#ifdef HAVE_FEATURE1
+    printf("Hello Feature1 \n");
+#endif
+
+#ifdef HAVE_FEATURE2
+    printf("Hello Feature2 \n");
+#endif
+#ifdef HAVE_FEATURE3
+    printf("Hello Feature3 \n");
+#endif
+
+#ifdef HAVE_FOO
+    printf("Hello Foo \n");
+#endif
     return 0;
 }
